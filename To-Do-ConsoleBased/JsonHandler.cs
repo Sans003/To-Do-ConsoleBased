a7658e3178b7870eedbeadd82df4dfde37787a5b
Sans003 <peanutstv1@gmail.com>
2023-08-30 13:30:44 +0200

    cool

---
+++ To-Do-ConsoleBased/JsonHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_Do_ConsoleBased
+{
+    internal class JsonHandler
+    {
+        string _jsonTitle = "data.json";
+        string _jsonInnerTitle = "ToDoItems";
+        string _itemTitle;
+        int _priority;
+        bool _done = false;
+
+        public JsonHandler(string? title, int priority)
+        {
+            this._itemTitle = title;
+            this._priority = priority;
+            Console.WriteLine($"You have created '{_itemTitle}' with priority {_priority}, which is done = {_done}");
+            Console.ReadLine();
+        }
+    }
+}
--- To-Do-ConsoleBased/Program.cs
+++ To-Do-ConsoleBased/Program.cs
@@ -1,36 +1,43 @@
 ﻿using System;
+using System.Reflection;
+using Newtonsoft.Json;
+using To_Do_ConsoleBased;
 
 namespace ToDoConsoleBased
 {
-    class ToDoList
+    class Program
     {
         static void Main(string[] args)
         {
-            MainMenu();
+            ToDoList.MainMenu();
         }
-
-        private static void MainMenu()
+    }
+    class ToDoList
+    {
+        public string title;
+        public int priority;
+        public static void MainMenu()
         {
             while (true)
             {
                 Console.Clear();
-                Console.Write(@"""
-    Welcome to your personal To-Do-List!
+                Console.Write(@"
+                Welcome to your personal To-Do-List!
 
-    select from the following actions:
+                select from the following actions:
 
-    1. list existing to do items
-    2. create new item
-    3. delete existing item
-    4. exit
-    """);
+                1. list existing to do items
+                2. create new item
+                3. delete existing item
+                4. exit
+                ");
                 switch (Convert.ToInt32(Console.ReadLine()))
                 {
                     case 1:
                         Console.WriteLine("placeholder");
                         break;
                     case 2:
-                        Console.WriteLine("placeholder");
+                        createItem();
                         break;
                     case 3:
                         Console.WriteLine("another placeholder");
@@ -43,6 +50,32 @@ namespace ToDoConsoleBased
                         break;
                 }
             }
+        }
+        public static void createItem()
+        {
+            Console.WriteLine("What do you want to do?");
+            string title = Console.ReadLine();
+            Console.WriteLine($"What priority does '{title}' have? (1-3)");
+            var choice = Console.ReadLine();
+            bool priorityCheck = false;
+            int priority = 0;
+            while (priorityCheck == false) {
+                switch (choice)
+                {
+                    case "1":
+                    case "2":
+                    case "3":
+                        priority = Convert.ToInt32(choice);
+                        priorityCheck = true;
+                        break;
+                    default:
+                        Console.WriteLine("please enter a valid integer.");
+                        choice = Console.ReadLine();
+                        break;
+                    }
+            JsonHandler jsonItem = new(title, priority);
+            }
+
         }
     }
 }
