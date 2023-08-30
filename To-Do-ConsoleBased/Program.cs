c9bb745c5267a7cd0b7e9f4992806a1a35c3da71
Sans003 <peanutstv1@gmail.com>
2023-08-30 12:35:46 +0200

    Add project files.

---
+++ To-Do-ConsoleBased/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ToDoConsoleBased
+{
+    class ToDoList
+    {
+        static void Main(string[] args)
+        {
+            MainMenu();
+        }
+
+        private static void MainMenu()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.Write(@"""
+    Welcome to your personal To-Do-List!
+
+    select from the following actions:
+
+    1. list existing to do items
+    2. create new item
+    3. delete existing item
+    4. exit
+    """);
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        Console.WriteLine("placeholder");
+                        break;
+                    case 2:
+                        Console.WriteLine("placeholder");
+                        break;
+                    case 3:
+                        Console.WriteLine("another placeholder");
+                        break;
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("no statement please try again");
+                        break;
+                }
+            }
+        }
+    }
+}
